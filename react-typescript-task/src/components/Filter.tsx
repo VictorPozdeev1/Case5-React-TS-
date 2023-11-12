d5c79dae64e7b2478eed59c3a58d226c0f88bf99
Victor Pozdeev <victorpozdeev@gmail.com>
2023-11-12 23:53:12 +0400

    add vehicles filtering

--- react-typescript-task/src/components/Filter.tsx
+++ react-typescript-task/src/components/Filter.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { VehicleFilter } from "../data/vehicles/contracts";
+import { VehicleTypeSelect } from "./VehicleTypeSelect";
 
-export class Filter extends React.Component {
-    render(): React.ReactNode {
-        return <div/>;
-    }
-}
+export const Filter: React.FC<{ value: VehicleFilter, onChange: (value: VehicleFilter) => void }> = ({ value, onChange }) => (
+    <div>
+        <input type="text" value={value.title} onChange={e => onChange({ ...value, title: e.target.value })} />
+        <VehicleTypeSelect value={value.type} onChange={type => onChange({ ...value, type })} />
+    </div>
+)
